ee36ee4639ad42e78cefc7d5a3616f912f18f532
preyelg <geeftedpl@gmail.com>
2025-05-26 02:31:19 -0500

    testing

--- files/app.js
+++ files/app.js
@@ -1,15 +1,36 @@
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-io.on('connection', socket => {
-    console.log('User connected');
-    socket.on('disconnect', () => console.log('User disconnected'));
+// Serve static files (e.g., index.html, styles.css)
+app.use(express.static(__dirname));
+
+// Serve index.html on root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(3000, () => console.log('Chat app running on port 3000'));
+// Handle WebSocket connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for chat messages
+  socket.on('chat message', (msg) => {
+    // Broadcast message to all connected clients
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Start server
+server.listen(3000, () => {
+  console.log('Chat app running on port 3000');
+});

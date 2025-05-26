5426e5783a7aebff80292ca0985e24c90823517c
preyelg <geeftedpl@gmail.com>
2025-05-26 12:47:07 -0500

    update

--- files/app.js
+++ files/app.js
@@ -1,15 +1,20 @@
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files from the same directory
+app.use(express.static(__dirname));
+
+// Socket.IO connection
 io.on('connection', socket => {
     console.log('User connected');
     socket.on('disconnect', () => console.log('User disconnected'));
 });
 
+// Start server
 server.listen(3000, () => console.log('Chat app running on port 3000'));
